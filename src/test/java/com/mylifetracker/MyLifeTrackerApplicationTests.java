9055b2aea1c0d0116d8908a96f985837d7094b3b
ngaikwad3 <ngaikwad3@dxc.com>
2020-08-30 15:25:36 +0530

    Initial Commit

---
+++ src/test/java/com/mylifetracker/MyLifeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylifetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLifeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
